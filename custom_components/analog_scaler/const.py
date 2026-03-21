236d9f34b8a0835556651841c58f48aa8f1997bc
koburg <allan@frederiksen.pro>
2026-03-21 14:25:03 +0100

    Create const.py

---
+++ custom_components/analog_scaler/const.py
@@ -0,0 +1,11 @@
+DOMAIN = "analog_scaler"
+
+CONF_SOURCE = "source"
+CONF_MIN_ANALOG = "min_analog"
+CONF_MAX_ANALOG = "max_analog"
+CONF_MIN_LIMIT = "min_limit"
+CONF_MAX_LIMIT = "max_limit"
+
+CONF_UNIT = "unit"
+CONF_DEVICE_CLASS = "device_class"
+CONF_STATE_CLASS = "state_class"
