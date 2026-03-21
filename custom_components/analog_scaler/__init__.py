a3d8fefcdfc378d7c83f161c925afbebfbcd2865
koburg <allan@frederiksen.pro>
2026-03-21 14:21:35 +0100

    Update __init__.py

--- custom_components/analog_scaler/__init__.py
+++ custom_components/analog_scaler/__init__.py
@@ -1,1 +1,16 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
+from .const import DOMAIN
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    hass.data.setdefault(DOMAIN, {})
+
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
